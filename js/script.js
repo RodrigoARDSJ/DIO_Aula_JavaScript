5f6212692fcb8e5d684723941e98859bbcf64682
RodrigoARDSJ <rodrigoalerez@gmail.com>
2021-01-22 19:24:51 -0300

    Aulas de Js da Digital Innovation One

---
+++ js/script.js
@@ -0,0 +1,90 @@
+function btn() {
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado em clicar</b>";
+}
+
+
+document.getElementById("btn-pagina").addEventListener("click", function () {
+    //  window.open("https://getbootstrap.com/docs/5.0/components/button-group/")
+    window.location.href = "https://getbootstrap.com/docs/5.0/components/button-group/"
+});
+
+document.getElementById("mousemove"),addEventListener("mouseover", function () {
+    document.getElementById("mousemove").innerHTML = "Obrigado por trocar"
+})
+
+document.getElementById("mousemove"),addEventListener("mouseout", function () {
+    document.getElementById("mousemove").innerHTML = "Passe o mouse aqui"
+})
+// function soma(n1 , n2) {
+//     return n1 + n2    
+// }
+
+// function validaIdade(idade) {
+//     var validar;
+//     if (idade >= 28) {
+//     validar = true;
+//     } else {
+//         validar = false;
+//     }
+//     return validar;
+
+// }
+// var idade= prompt("Digite a sua idade: ")
+// console.log(validaIdade(idade));
+// console.log(soma(2, 7))
+
+
+// var  d = new Date();
+// console.log(d.getMonth() + 1);
+// console.log(d.getMinutes())
+// console.log(d.getDay())
+
+
+// var i = 0;
+// while (i <= 5) {
+//     console.log(i);
+//     i = i + 1;
+
+// }
+
+// for (let index = 0; index <= 5; index++) {
+//     console.log(index)
+
+// }
+
+// var idade = prompt("Digite a sua idade")
+// if (idade >= 18) {
+//     console.log("Maior de idade")
+// } else {
+//     console.log("Menor de idade")
+// }
+
+// var fruta = [{ nome: "Maçã", cor: "Vermelho" }, { nome: "uva", cor: "roxo" }]
+// console.log(fruta);
+// console.log(fruta[1]);
+// var fruta = {nome: "Maçã", Cor: "Vermelho"}
+// console.log(fruta);
+// var lista = ["Maçã", "Pera", "Banana"]
+// console.log(lista);
+// lista.push("uva");
+// console.log(lista);
+// lista.pop();
+// console.log(lista);
+// console.log(lista.length);
+// console.log(lista.reverse());
+// console.log(lista.toString());
+// console.log(lista.join(" - "));
+// var nome = "Rodrigo Alexander"
+// var idade = 20
+// var idade2 = 15
+// var frase = "Japão é o melhor time do mundo."
+
+//  console.log(nome + " tem " + idade + " anos")
+//  console.log(idade + idade2)
+
+// console.log(nome + " tem " + idade + " anos");
+// console.log(idade + idade2);
+// console.log(frase);
+// console.log(frase.replace("Japão", "Brasil"));
+// console.log(frase.toUpperCase);
+// console.log(frase.toLowerCase);
